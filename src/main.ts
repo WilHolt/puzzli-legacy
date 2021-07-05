191ef7b56b04f88457319baf449a1e23b74a196f
Wil <joswilder46@gmail.com>
2021-07-05 03:07:26 -0300

    remove pzl components reference

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as fromPuzzl from 'puzzl-components/dist/stencil/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -11,4 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-fromPuzzl.defineCustomElements();
